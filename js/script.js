6c126b21fa620951b7d19720d06d075d091881e1
AkbarjonTojiyev <tojiyev.akbarjon@gmail.com>
2020-08-20 01:34:47 +0500

    Make modal interactive with JS

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-// scripts
+// nav-list
 const elHamburger = document.getElementById('jsHamburger')
-
 const elBurger = document.getElementById('burger')
 
 elHamburger.onclick = () => {
-	
 	burger.classList.toggle('burger-on')
+}
 
+// modal
+const elModal = document.getElementById('jsModal')
+const elModalBtn = document.getElementById('jsModalBtn')
+
+elModalBtn.onclick = () => {
+	elModal.classList.add('modal-off')
 }
