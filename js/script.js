8279857ee94c73e342cc265397bf5782816ce550
AkbarjonTojiyev <tojiyev.akbarjon@gmail.com>
2020-08-20 01:19:53 +0500

    Add nav-list

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 // scripts
+const elHamburger = document.getElementById('jsHamburger')
+
+const elBurger = document.getElementById('burger')
+
+elHamburger.onclick = () => {
+	
+	burger.classList.toggle('burger-on')
+
+}
