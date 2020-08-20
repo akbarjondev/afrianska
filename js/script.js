4f28ae87597166285767f8418502fae380736fa9
AkbarjonTojiyev <tojiyev.akbarjon@gmail.com>
2020-08-20 13:54:53 +0500

    Make modal interactive

--- js/script.js
+++ js/script.js
@@ -10,6 +10,11 @@ elHamburger.onclick = () => {
 const elModal = document.getElementById('jsModal')
 const elModalBtn = document.getElementById('jsModalBtn')
 
+if(localStorage.getItem('_modal')) {
+	elModal.classList.add('modal-off')
+}
+
 elModalBtn.onclick = () => {
 	elModal.classList.add('modal-off')
+	localStorage.setItem('_modal', 'done')
 }
